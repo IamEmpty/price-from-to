2618012ddcfdb0fbeeca31de483f82ef4e137037
IamEmpty <evolution.is.already.underway@gmail.com>
2016-07-30 02:45:20 +0300

    add value

--- build/index.js
+++ build/index.js
@@ -8,3 +8,10 @@ function showDropdown() {
   document.querySelector('.price-range-dropdown__input').focus();
   return false
 }
+
+document.querySelector('.price-range__list').addEventListener('click', choose);
+
+function choose(e) {
+  e.preventDefault();
+  console.log(e.target.value);
+}
