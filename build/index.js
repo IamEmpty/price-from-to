5ec264cd571abc5dc80a646c8d0976d7134361d8
IamEmpty <evolution.is.already.underway@gmail.com>
2016-07-30 03:08:09 +0300

    change js

--- build/index.js
+++ build/index.js
@@ -3,15 +3,16 @@ element.classList.add('hide');
 
 document.querySelector('.price-range-button').addEventListener('click', showDropdown);
 
-function showDropdown() {
+function showDropdown(e) {
   document.querySelector('.price-range-dropdown').classList.toggle('hide');
   document.querySelector('.price-range-dropdown__input').focus();
-  return false
+  e.preventDefault();
 }
 
-document.querySelector('.price-range__list').addEventListener('click', choose);
+document.querySelector('.price-range__column .price-range__list').addEventListener('click', choose);
 
 function choose(e) {
   e.preventDefault();
-  console.log(e.target.value);
+  this.previousSibling.value = e.target.value;
+  document.querySelector('#price-range-finish').focus();
 }
